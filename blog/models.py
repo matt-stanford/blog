8301d384a7063b04c61af9aa60cf574cdff5ab15
matt-stanford <mjfstanford@gmail.com>
2020-05-08 19:16:53 +0100

    added all crud

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -10,5 +11,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
 
     objects = models.Manager()
